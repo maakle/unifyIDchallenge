ba4e1d546e0c74b58da9c76a2b1789d6dd47abff
Mathias Klenk <mathiasklenk@gmx.de>
2017-06-06 15:06:38 -0700

    Initial Commit

---
+++ unifyIDchallengeTests/unifyIDchallengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  unifyIDchallengeTests.swift
+//  unifyIDchallengeTests
+//
+//  Created by Mathias Klenk on 06/06/17.
+//  Copyright © 2017 Mathias Klenk. All rights reserved.
+//
+
+import XCTest
+@testable import unifyIDchallenge
+
+class unifyIDchallengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
