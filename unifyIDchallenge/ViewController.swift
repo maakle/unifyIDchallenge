0fa1ad8043472e219c2c96a167da5e9f57dd27c6
Mathias Klenk <mathiasklenk@gmx.de>
2017-06-06 16:39:51 -0700

    solved assignment with all requirements

--- unifyIDchallenge/ViewController.swift
+++ unifyIDchallenge/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import KeychainSwift
 
 class ViewController: UIViewController,  UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    //Outlets
     @IBOutlet weak var authenticateButton: UIButton!
     
+    //Objects
+    let keychain = KeychainSwift()
+    
+    //Variables
+    private var index = 0
+    private var arrayForImages = [UIImage]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,6 +60,24 @@ class ViewController: UIViewController,  UIImagePickerControllerDelegate, UINavi
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         print("Picture has been taken")
         let imageTaken = info[UIImagePickerControllerOriginalImage] as! UIImage
+        self.arrayForImages.append(imageTaken)
+        
+        if self.arrayForImages.count == 10 {
+            transformToDataAndSaveSecurely(array: self.arrayForImages)
+        }
+    }
+    
+    func transformToDataAndSaveSecurely(array: [UIImage]){
+        for i in 0..<10 {
+            let image = array[i]
+            if let data = UIImagePNGRepresentation(image) {
+                keychain.set(data, forKey: "Picture: \(i+1)")
+                print("Picture saved")
+                if i == 0 {
+                    self.dismiss(animated: true, completion: nil)
+                }
+            }
+        }
     }
     
     //MARK: -  UI
